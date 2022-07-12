6b9bb83e274ee235c3801a4f7eb0fdae0c5def55
Hasit Parmar <69752829+hasit73@users.noreply.github.com>
2022-07-12 21:52:07 +0530

    Update celery file

--- real_time_celery/celery.py
+++ real_time_celery/celery.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 app = Celery(
     "test_celery",
-    broker="amqp://hasit73:9825300502@localhost/hasit_celery",
+    broker="amqp://hasit73:****@localhost/hasit_celery",
     backend="rpc://",
     include=["real_time_celery.tasks"],
 )
